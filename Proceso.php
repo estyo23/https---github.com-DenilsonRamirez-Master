1a400b0c7030747489ce4410ccb783d53e86a883
estyo23 <jl442102@gmail.com>
2022-08-18 18:45:24 -0600

    first commit

---
+++ Proceso.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        Se Recibieron Los Siguientes datos
+    </h2>
+    <?php
+    echo $_POST['pnumero'];
+    echo $_POST['snumero'];
+    echo $_POST['Operacion'];
+    echo "hice un cambio";
+    ?>
+</body>
+</html>
